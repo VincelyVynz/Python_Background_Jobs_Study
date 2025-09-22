02f85044ad7c9fd77b0e2a9775b023b57f5a9ddd
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-22 11:06:17 +0530

    Update threading_study.py

--- threading_study.py
+++ threading_study.py
@@ -3,9 +3,9 @@ import threading
 import time
 
 def download_files(url):
-    print("Starting download")
-    time.sleep(5)
-    print("Download finished")
+    print(f"Starting download {url}")
+    time.sleep(2)
+    print(f"Download finished {url}")
 
 t1 = threading.Thread(target=download_files, args = ("file_1",))
 t2 = threading.Thread(target=download_files, args = ("file_2",))
