6c795d2b42dcb0d15d36361bc79c70144c6ea534
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-22 13:59:07 +0530

    Add lock method to multi_file_downloader_with_threading.py

--- multi_file_downloader_with_threading.py
+++ multi_file_downloader_with_threading.py
@@ -1,12 +1,15 @@
 import requests, threading
 
+print_lock = threading.Lock()
+
 def download_file(url):
     image_name = url.split('/')[-1]
     if image_name == "":
         image_name = url.split('/')[-2]
     image_name = image_name.split('?')[0]
 
-    print(f'Download {image_name} started...')
+    with print_lock:
+        print(f'Download {image_name} started...')
 
     response = requests.get(url)
     response.raise_for_status()
@@ -14,7 +17,8 @@ def download_file(url):
     if response.status_code == 200:
         with open(f"downloads/{image_name}", "wb") as file:
             file.write(response.content)
-        print(f'Download {image_name} completed.')
+        with print_lock:
+            print(f'Download {image_name} completed.')
     else:
         print(response.status_code)
 
